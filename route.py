19e01507d96a133a37bd57701dd267498a05d7d6
Jek <jchoo@splunk.com>
2021-11-25 17:28:20 +0800

    init commit from k. oat

---
+++ route.py
@@ -0,0 +1,57 @@
+from flask import Flask
+from flask_restful import Api
+from flask_restful.utils import cors
+
+#   Need to check that app run under uWSGI context
+#   In case of pytest we run out of uWSGI context then it can't
+#   import uwsgi
+import uwsgidecorators
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from splunk_otel.tracing import start_tracing
+
+
+from resource.health_check import HealthCheck
+
+
+def add_routes(router):
+    router.add_resource(HealthCheck, "/healthz")
+
+
+app = Flask(__name__)
+
+
+@uwsgidecorators.postfork
+def setup_tracing():
+    start_tracing()
+    # Instrument the Flask app instance explicitly
+    FlaskInstrumentor().instrument_app(app)
+
+
+api = Api(app)
+api.decorators = [
+    cors.crossdomain(
+        origin="*",
+        headers=[
+            "accept",
+            "Content-Type",
+            "X-Requested-With",
+            "publicToken",
+            "device",
+            "privateToken",
+            "memberId",
+            "uuid",
+            "admin-email",
+        ],
+        methods=["OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH"],
+        max_age=300,
+    )
+]
+add_routes(api)
+
+
+def start_app():
+    app.run(host="0.0.0.0", port=5004, threaded=True)
+
+
+if __name__ == "__main__":
+    start_app()
